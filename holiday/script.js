b0dbae047de511e0ad24ad0b3606633084446406
xvdemo1 <xvdemo1@gmail.com>
2025-07-08 10:50:46 +0200

    Added Holiday Link Generator
    Added Holiday Link Generator

---
+++ holiday/script.js
@@ -0,0 +1,100 @@
+// Placeholder JSON data
+const pages = [{"name":"Homepage","languages":[{"code":"en","id":"2522358"}]},{"name":"eSIM Africa","languages":[{"code":"en","id":"2715311"}]},{"name":"eSIM Asia","languages":[{"code":"en","id":"2715305"}]},{"name":"eSIM Caribbean","languages":[{"code":"en","id":"2715310"}]},{"name":"eSIM for Afghanistan","languages":[{"code":"en","id":"2655712"}]},{"name":"eSIM for Albania","languages":[{"code":"en","id":"2655713"}]},{"name":"eSIM for Algeria","languages":[{"code":"en","id":"2655714"}]},{"name":"eSIM for Andorra","languages":[{"code":"en","id":"2655715"}]},{"name":"eSIM for Angola","languages":[{"code":"en","id":"2655716"}]},{"name":"eSIM for Anguilla","languages":[{"code":"en","id":"2655717"}]},{"name":"eSIM for Antigua and Barbuda","languages":[{"code":"en","id":"2655718"}]},{"name":"eSIM for Argentina","languages":[{"code":"en","id":"2569280"}]},{"name":"eSIM for Armenia","languages":[{"code":"en","id":"2655719"}]},{"name":"eSIM for Aruba","languages":[{"code":"en","id":"2655720"}]},{"name":"eSIM for Australia","languages":[{"code":"en","id":"2569281"}]},{"name":"eSIM for Austria","languages":[{"code":"en","id":"2655721"}]},{"name":"eSIM for Azerbaijan","languages":[{"code":"en","id":"2655722"}]},{"name":"eSIM for Bahamas","languages":[{"code":"en","id":"2655724"}]},{"name":"eSIM for Bahrain","languages":[{"code":"en","id":"2655725"}]},{"name":"eSIM for Bangladesh","languages":[{"code":"en","id":"2655726"}]},{"name":"eSIM for Barbados","languages":[{"code":"en","id":"2655727"}]},{"name":"eSIM for Belarus","languages":[{"code":"en","id":"2655728"}]},{"name":"eSIM for Belgium","languages":[{"code":"en","id":"2655729"}]},{"name":"eSIM for Belize","languages":[{"code":"en","id":"2655730"}]},{"name":"eSIM for Benin","languages":[{"code":"en","id":"2655731"}]},{"name":"eSIM for Bermuda","languages":[{"code":"en","id":"2655732"}]},{"name":"eSIM for Bhutan","languages":[{"code":"en","id":"2655733"}]},{"name":"eSIM for Bolivia","languages":[{"code":"en","id":"2655734"}]},{"name":"eSIM for Bosnia and Herzegovina","languages":[{"code":"en","id":"2655735"}]},{"name":"eSIM for Botswana","languages":[{"code":"en","id":"2655736"}]},{"name":"eSIM for Brazil","languages":[{"code":"en","id":"2569282"}]},{"name":"eSIM for British Virgin Islands","languages":[{"code":"en","id":"2655737"}]},{"name":"eSIM for Brunei","languages":[{"code":"en","id":"2655738"}]},{"name":"eSIM for Bulgaria","languages":[{"code":"en","id":"2655739"}]},{"name":"eSIM for Burkina Faso","languages":[{"code":"en","id":"2655740"}]},{"name":"eSIM for Cambodia","languages":[{"code":"en","id":"2648491"}]},{"name":"eSIM for Cameroon","languages":[{"code":"en","id":"2655741"}]},{"name":"eSIM for Canada","languages":[{"code":"en","id":"2569283"}]},{"name":"eSIM for Cape Verde","languages":[{"code":"en","id":"2655742"}]},{"name":"eSIM for Cayman Islands","languages":[{"code":"en","id":"2655743"}]},{"name":"eSIM for Central African Republic","languages":[{"code":"en","id":"2655744"}]},{"name":"eSIM for Chad","languages":[{"code":"en","id":"2655745"}]},{"name":"eSIM for Chile","languages":[{"code":"en","id":"2569284"}]},{"name":"eSIM for China","languages":[{"code":"en","id":"2569285"}]},{"name":"eSIM for Colombia","languages":[{"code":"en","id":"2655746"}]},{"name":"eSIM for Costa Rica","languages":[{"code":"en","id":"2569286"}]},{"name":"eSIM for Croatia","languages":[{"code":"en","id":"2655747"}]},{"name":"eSIM for Curacao","languages":[{"code":"en","id":"2655748"}]},{"name":"eSIM for Cyprus","languages":[{"code":"en","id":"2655749"}]},{"name":"eSIM for Czech Republic","languages":[{"code":"en","id":"2655750"}]},{"name":"eSIM for Democratic Republic of the Congo","languages":[{"code":"en","id":"2655751"}]},{"name":"eSIM for Denmark","languages":[{"code":"en","id":"2655752"}]},{"name":"eSIM for Dominica","languages":[{"code":"en","id":"2655753"}]},{"name":"eSIM for Dominican Republic","languages":[{"code":"en","id":"2655754"}]},{"name":"eSIM for Ecuador","languages":[{"code":"en","id":"2655755"}]},{"name":"eSIM for Egypt","languages":[{"code":"en","id":"2569287"}]},{"name":"eSIM for El Salvador","languages":[{"code":"en","id":"2655756"}]},{"name":"eSIM for Estonia","languages":[{"code":"en","id":"2655757"}]},{"name":"eSIM for Ethiopia","languages":[{"code":"en","id":"2655758"}]},{"name":"eSIM for Europe","languages":[{"code":"en","id":"2569288"}]},{"name":"eSIM for Falkland Islands","languages":[{"code":"en","id":"2655759"}]},{"name":"eSIM for Faroe Islands","languages":[{"code":"en","id":"2655760"}]},{"name":"eSIM for Fiji","languages":[{"code":"en","id":"2655761"}]},{"name":"eSIM for Finland","languages":[{"code":"en","id":"2655762"}]},{"name":"eSIM for France","languages":[{"code":"en","id":"2655764"}]},{"name":"eSIM for French Guiana","languages":[{"code":"en","id":"2655765"}]},{"name":"eSIM for French Polynesia","languages":[{"code":"en","id":"2655766"}]},{"name":"eSIM for Gabon","languages":[{"code":"en","id":"2655767"}]},{"name":"eSIM for Gambia","languages":[{"code":"en","id":"2655768"}]},{"name":"eSIM for Georgia","languages":[{"code":"en","id":"2655769"}]},{"name":"eSIM for Germany","languages":[{"code":"en","id":"2648489"}]},{"name":"eSIM for Ghana","languages":[{"code":"en","id":"2655770"}]},{"name":"eSIM for Gibraltar","languages":[{"code":"en","id":"2655771"}]},{"name":"eSIM for Greece","languages":[{"code":"en","id":"2569289"}]},{"name":"eSIM for Greenland","languages":[{"code":"en","id":"2655772"}]},{"name":"eSIM for Grenada","languages":[{"code":"en","id":"2655773"}]},{"name":"eSIM for Guadeloupe","languages":[{"code":"en","id":"2655774"}]},{"name":"eSIM for Guam","languages":[{"code":"en","id":"2655775"}]},{"name":"eSIM for Guatemala","languages":[{"code":"en","id":"2655776"}]},{"name":"eSIM for Guernsey","languages":[{"code":"en","id":"2655777"}]},{"name":"eSIM for Guinea","languages":[{"code":"en","id":"2655778"}]},{"name":"eSIM for Guinea-Bissau","languages":[{"code":"en","id":"2655779"}]},{"name":"eSIM for Guyana","languages":[{"code":"en","id":"2655780"}]},{"name":"eSIM for Haiti","languages":[{"code":"en","id":"2655781"}]},{"name":"eSIM for Honduras","languages":[{"code":"en","id":"2655782"}]},{"name":"eSIM for Hong Kong","languages":[{"code":"en","id":"2569290"}]},{"name":"eSIM for Hungary","languages":[{"code":"en","id":"2655783"}]},{"name":"eSIM for Iceland","languages":[{"code":"en","id":"2655784"}]},{"name":"eSIM for India","languages":[{"code":"en","id":"2569291"}]},{"name":"eSIM for Indonesia","languages":[{"code":"en","id":"2569292"}]},{"name":"eSIM for Iraq","languages":[{"code":"en","id":"2655785"}]},{"name":"eSIM for Ireland","languages":[{"code":"en","id":"2655786"}]},{"name":"eSIM for Isle of Man","languages":[{"code":"en","id":"2655787"}]},{"name":"eSIM for Israel","languages":[{"code":"en","id":"2655788"}]},{"name":"eSIM for Italy","languages":[{"code":"en","id":"2655789"}]},{"name":"eSIM for Jamaica","languages":[{"code":"en","id":"2655790"}]},{"name":"eSIM for Japan","languages":[{"code":"en","id":"2569293"}]},{"name":"eSIM for Jersey","languages":[{"code":"en","id":"2655791"}]},{"name":"eSIM for Jordan","languages":[{"code":"en","id":"2655793"}]},{"name":"eSIM for Kazakhstan","languages":[{"code":"en","id":"2655794"}]},{"name":"eSIM for Kenya","languages":[{"code":"en","id":"2655795"}]},{"name":"eSIM for Kiribati","languages":[{"code":"en","id":"2655796"}]},{"name":"eSIM for Kosovo","languages":[{"code":"en","id":"2655797"}]},{"name":"eSIM for Kuwait","languages":[{"code":"en","id":"2655798"}]},{"name":"eSIM for Kyrgyzstan","languages":[{"code":"en","id":"2655799"}]},{"name":"eSIM for Laos","languages":[{"code":"en","id":"2655800"}]},{"name":"eSIM for Latvia","languages":[{"code":"en","id":"2655801"}]},{"name":"eSIM for Lesotho","languages":[{"code":"en","id":"2655802"}]},{"name":"eSIM for Liechtenstein","languages":[{"code":"en","id":"2655803"}]},{"name":"eSIM for Lithuania","languages":[{"code":"en","id":"2655804"}]},{"name":"eSIM for Luxembourg","languages":[{"code":"en","id":"2655805"}]},{"name":"eSIM for Macau","languages":[{"code":"en","id":"2655806"}]},{"name":"eSIM for Macedonia","languages":[{"code":"en","id":"2655807"}]},{"name":"eSIM for Madagascar","languages":[{"code":"en","id":"2655808"}]},{"name":"eSIM for Malawi","languages":[{"code":"en","id":"2655809"}]},{"name":"eSIM for Malaysia","languages":[{"code":"en","id":"2569295"}]},{"name":"eSIM for Maldives","languages":[{"code":"en","id":"2655810"}]},{"name":"eSIM for Mali","languages":[{"code":"en","id":"2655811"}]},{"name":"eSIM for Malta","languages":[{"code":"en","id":"2655812"}]},{"name":"eSIM for Martinique","languages":[{"code":"en","id":"2655813"}]},{"name":"eSIM for Mauritania","languages":[{"code":"en","id":"2655814"}]},{"name":"eSIM for Mauritius","languages":[{"code":"en","id":"2655815"}]},{"name":"eSIM for Mayotte","languages":[{"code":"en","id":"2655817"}]},{"name":"eSIM for Mexico","languages":[{"code":"en","id":"2569296"}]},{"name":"eSIM for Moldova","languages":[{"code":"en","id":"2655818"}]},{"name":"eSIM for Monaco","languages":[{"code":"en","id":"2655819"}]},{"name":"eSIM for Mongolia","languages":[{"code":"en","id":"2655820"}]},{"name":"eSIM for Montenegro","languages":[{"code":"en","id":"2655821"}]},{"name":"eSIM for Montserrat","languages":[{"code":"en","id":"2655822"}]},{"name":"eSIM for Morocco","languages":[{"code":"en","id":"2569297"}]},{"name":"eSIM for Mozambique","languages":[{"code":"en","id":"2655824"}]},{"name":"eSIM for Nauru","languages":[{"code":"en","id":"2655825"}]},{"name":"eSIM for Nepal","languages":[{"code":"en","id":"2655826"}]},{"name":"eSIM for Netherlands","languages":[{"code":"en","id":"2655827"}]},{"name":"eSIM for Netherlands Antilles","languages":[{"code":"en","id":"2655828"}]},{"name":"eSIM for New Zealand","languages":[{"code":"en","id":"2569298"}]},{"name":"eSIM for Nicaragua","languages":[{"code":"en","id":"2655829"}]},{"name":"eSIM for Niger","languages":[{"code":"en","id":"2655830"}]},{"name":"eSIM for Nigeria","languages":[{"code":"en","id":"2655831"}]},{"name":"eSIM for Northern Mariana Islands","languages":[{"code":"en","id":"2655832"}]},{"name":"eSIM for Norway","languages":[{"code":"en","id":"2655833"}]},{"name":"eSIM for Oman","languages":[{"code":"en","id":"2655834"}]},{"name":"eSIM for Pakistan","languages":[{"code":"en","id":"2655835"}]},{"name":"eSIM for Palau","languages":[{"code":"en","id":"2655836"}]},{"name":"eSIM for Palestine","languages":[{"code":"en","id":"2655837"}]},{"name":"eSIM for Panama","languages":[{"code":"en","id":"2655838"}]},{"name":"eSIM for Papua New Guinea","languages":[{"code":"en","id":"2655839"}]},{"name":"eSIM for Paraguay","languages":[{"code":"en","id":"2655840"}]},{"name":"eSIM for Peru","languages":[{"code":"en","id":"2655841"}]},{"name":"eSIM for Philippines","languages":[{"code":"en","id":"2569299"}]},{"name":"eSIM for Poland","languages":[{"code":"en","id":"2648490"}]},{"name":"eSIM for Portugal","languages":[{"code":"en","id":"2655842"}]},{"name":"eSIM for Puerto Rico","languages":[{"code":"en","id":"2655843"}]},{"name":"eSIM for Qatar","languages":[{"code":"en","id":"2655844"}]},{"name":"eSIM for Republic of the Congo","languages":[{"code":"en","id":"2655845"}]},{"name":"eSIM for Reunion","languages":[{"code":"en","id":"2655846"}]},{"name":"eSIM for Romania","languages":[{"code":"en","id":"2655847"}]},{"name":"eSIM for Russia","languages":[{"code":"en","id":"2655848"}]},{"name":"eSIM for Rwanda","languages":[{"code":"en","id":"2655849"}]},{"name":"eSIM for Saint Barthelemy","languages":[{"code":"en","id":"2655850"}]},{"name":"eSIM for Saint Kitts and Nevis","languages":[{"code":"en","id":"2655851"}]},{"name":"eSIM for Saint Lucia","languages":[{"code":"en","id":"2655852"}]},{"name":"eSIM for Saint Martin","languages":[{"code":"en","id":"2655853"}]},{"name":"eSIM for Saint Pierre and Miquelon","languages":[{"code":"en","id":"2655854"}]},{"name":"eSIM for Saint Vincent and the Grenadines","languages":[{"code":"en","id":"2655855"}]},{"name":"eSIM for Samoa","languages":[{"code":"en","id":"2655857"}]},{"name":"eSIM for San Marino","languages":[{"code":"en","id":"2655858"}]},{"name":"eSIM for Saudi Arabia","languages":[{"code":"en","id":"2655859"}]},{"name":"eSIM for Senegal","languages":[{"code":"en","id":"2655860"}]},{"name":"eSIM for Serbia","languages":[{"code":"en","id":"2655861"}]},{"name":"eSIM for Seychelles","languages":[{"code":"en","id":"2655862"}]},{"name":"eSIM for Shi Lanka","languages":[{"code":"en","id":"2569302"}]},{"name":"eSIM for Sierra Leone","languages":[{"code":"en","id":"2655863"}]},{"name":"eSIM for Singapore","languages":[{"code":"en","id":"2569300"}]},{"name":"eSIM for Slovakia","languages":[{"code":"en","id":"2655864"}]},{"name":"eSIM for Slovenia","languages":[{"code":"en","id":"2655865"}]},{"name":"eSIM for South Africa","languages":[{"code":"en","id":"2569301"}]},{"name":"eSIM for South Korea","languages":[{"code":"en","id":"2569294"}]},{"name":"eSIM for Spain","languages":[{"code":"en","id":"2655866"}]},{"name":"eSIM for Suriname","languages":[{"code":"en","id":"2655867"}]},{"name":"eSIM for Swaziland","languages":[{"code":"en","id":"2655868"}]},{"name":"eSIM for Sweden","languages":[{"code":"en","id":"2655869"}]},{"name":"eSIM for Switzerland","languages":[{"code":"en","id":"2655870"}]},{"name":"eSIM for Taiwan","languages":[{"code":"en","id":"2655872"}]},{"name":"eSIM for Tajikistan","languages":[{"code":"en","id":"2655873"}]},{"name":"eSIM for Tanzania","languages":[{"code":"en","id":"2648492"}]},{"name":"eSIM for Thailand","languages":[{"code":"en","id":"2569303"}]},{"name":"eSIM for Timor-Leste","languages":[{"code":"en","id":"2655874"}]},{"name":"eSIM for Togo","languages":[{"code":"en","id":"2655875"}]},{"name":"eSIM for Tonga","languages":[{"code":"en","id":"2655876"}]},{"name":"eSIM for Trinidad and Tobago","languages":[{"code":"en","id":"2655877"}]},{"name":"eSIM for Tunisia","languages":[{"code":"en","id":"2655878"}]},{"name":"eSIM for Turkey","languages":[{"code":"en","id":"2569304"}]},{"name":"eSIM for Turks and Caicos Islands","languages":[{"code":"en","id":"2655879"}]},{"name":"eSIM for U.S. Virgin Islands","languages":[{"code":"en","id":"2655880"}]},{"name":"eSIM for Uganda","languages":[{"code":"en","id":"2655881"}]},{"name":"eSIM for Ukraine","languages":[{"code":"en","id":"2655882"}]},{"name":"eSIM for United Arab Emirates","languages":[{"code":"en","id":"2569305"}]},{"name":"eSIM for Uruguay","languages":[{"code":"en","id":"2655883"}]},{"name":"eSIM for Uzbekistan","languages":[{"code":"en","id":"2655884"}]},{"name":"eSIM for Vanuatu","languages":[{"code":"en","id":"2655885"}]},{"name":"eSIM for Vatican City","languages":[{"code":"en","id":"2655886"}]},{"name":"eSIM for Venezuela","languages":[{"code":"en","id":"2655887"}]},{"name":"eSIM for Vietnam","languages":[{"code":"en","id":"2569308"}]},{"name":"eSIM for Yemen","languages":[{"code":"en","id":"2655888"}]},{"name":"eSIM for Zambia","languages":[{"code":"en","id":"2655889"}]},{"name":"eSIM Latin America","languages":[{"code":"en","id":"2715306"}]},{"name":"eSIM Middle East","languages":[{"code":"en","id":"2715307"}]},{"name":"eSIM North America","languages":[{"code":"en","id":"2715308"}]},{"name":"eSIM Ocenia","languages":[{"code":"en","id":"2715309"}]},{"name":"eSIM UK","languages":[{"code":"en","id":"2569306"}]},{"name":"eSIM USA","languages":[{"code":"en","id":"2569307"}]}];
+let selectedPageName = "";
+let selectedLanguageCode = "";
+
+// Get references to the select elements and generate button
+const pageSelector = document.getElementById("page-selector");
+const languageSelector = document.getElementById("language-selector");
+const generateBtn = document.getElementById("generate-btn");
+const result = document.getElementById("result");
+
+// Set default values
+document.getElementById("personal-id").value = "Your Impact ID";
+
+// Populate the page selector with options and select the first page
+for (let i = 0; i < pages.length; i++) {
+  const option = document.createElement("option");
+  option.value = pages[i].name;
+  option.text = pages[i].name;
+  pageSelector.appendChild(option);
+}
+pageSelector.selectedIndex = 0;
+
+// Update the language selector options and generate the URL on page load
+updateLanguageSelectorAndGenerateUrl();
+
+// Update the language selector options when the page selector changes
+pageSelector.addEventListener("change", function() {
+  updateLanguageSelectorAndGenerateUrl();
+});
+
+// Function to check if a value is numerical
+function isNumerical(value) {
+  return /^\d+$/.test(value);
+}
+
+// Modified function to update the language selector options and generate the URL
+function updateLanguageSelectorAndGenerateUrl() {
+  // Clear the language selector options
+  languageSelector.innerHTML = "";
+
+  // Find the selected page
+  selectedPageName = pageSelector.value;
+  const selectedPage = pages.find(function(page) {
+    return page.name === selectedPageName;
+  });
+
+  // Populate the language selector with options
+  for (let i = 0; i < selectedPage.languages.length; i++) {
+    const option = document.createElement("option");
+    option.value = selectedPage.languages[i].id;
+    option.text = selectedPage.languages[i].code;
+    languageSelector.appendChild(option);
+  }
+
+  // Set default language and generate URL
+  languageSelector.selectedIndex = 0;
+  generateBtn.click();
+}
+
+// Update selectedLanguageCode when the language selector changes
+languageSelector.addEventListener("change", function() {
+  selectedLanguageCode = languageSelector.value;
+});
+
+generateBtn.addEventListener("click", function() {
+  // Get the personal ID value and check if it's a numerical value
+  const personalId = document.getElementById("personal-id").value;
+  if (!isNumerical(personalId)) {
+    alert("Error: Impact ID must be a numerical value.");
+    return; // Stop URL generation if the personal ID is not valid
+  }
+
+  const selectedLanguage = languageSelector.options[languageSelector.selectedIndex];
+  const pageId = selectedLanguage.value;
+  const label4 = document.getElementById("label4").value;
+
+  let url = `${selectedPageName} (${selectedLanguage.text}): https://go.holiday.com/c/${personalId}/${pageId}/29571`;
+
+  if (label4 !== "") {
+    url += `?sharedid=${encodeURIComponent(label4)}`;
+  }
+
+  result.innerText = url;
+});
+
+function copyToClipboard() {
+  const resultText = result.innerText;
+  const urlStartIndex = resultText.indexOf("https://");
+  if (urlStartIndex >= 0) {
+    const url = resultText.substring(urlStartIndex);
+    const el = document.createElement('textarea');
+    el.value = url;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert("URL copied to clipboard: " + url);
+  }
+}
